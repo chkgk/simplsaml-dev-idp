071469651670dbeeab602722e24de09d4a493214
chkgk <koenig.kersting@gmail.com>
2025-03-27 18:50:45 +0100

    dev saml idp

---
+++ authsources.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = array(
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'user1:password' => array(
+            'email' => 'user1@example.com',
+        ),
+        'user2:password' => array(
+            'email' => 'user2@example.com',
+        ),
+        'user3:password' => array(
+            'email' => 'user3@example.com',
+        ),
+    ),
+
+);
+?>
